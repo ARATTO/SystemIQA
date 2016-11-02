6b5e42795666bb3c8577cde4e5f53a04a499d03c
Rodrigo <rodrigoxj32@hotmail.com>
2016-11-01 18:35:16 -0600

    tabla de porcentaje de notas

--- app/Http/Controllers/PorcentajeNotasController.php
+++ app/Http/Controllers/PorcentajeNotasController.php
@@ -48,72 +48,7 @@ class PorcentajeNotasController extends Controller
     public function store(PorcentajeNotasRequest $request){
 
 
-        $evaluacion[0] = $request->Evaluacion1;
-        $evaluacion[1] = $request->Evaluacion2;
-        $evaluacion[2] = $request->Evaluacion3;
-        $evaluacion[3] = $request->Evaluacion4;
-        $evaluacion[4] = $request->Evaluacion5;
-        $evaluacion[5] = $request->Evaluacion6;
-        $evaluacion[6] = $request->Evaluacion7;
-        $evaluacion[7] = $request->Evaluacion8;
-        $evaluacion[8] = $request->Evaluacion9;
-        $evaluacion[9] = $request->Evaluacion10;
-
-        $descripcion[0] = $request->descripcion1;
-        $descripcion[1] = $request->descripcion2;
-        $descripcion[2] = $request->descripcion3;
-        $descripcion[3] = $request->descripcion4;
-        $descripcion[4] = $request->descripcion5;
-        $descripcion[5] = $request->descripcion6;
-        $descripcion[6] = $request->descripcion7;
-        $descripcion[7] = $request->descripcion8;
-        $descripcion[8] = $request->descripcion9;
-        $descripcion[9] = $request->descripcion10;
-
-
-        $porcentaje =0;
-
-        for($i=0; $i<10; $i++){
-            $porcentaje+=$evaluacion[$i];
-        }
-
-
-
-        if ($porcentaje == 100) {
-            ///dd($request->all());
-                for ($i=0; $i<10 ; $i++) { 
-                    
-                    if ($evaluacion[$i] != "" && $evaluacion[$i]>0) {
-
-                        $evaluacionN =  new Evaluacion();
-                        $evaluacionN->porcentaje = $evaluacion[$i];
-                        $evaluacionN->descripcion = $descripcion[$i];
-                        $evaluacionN->materia_id = $request->id;
-
-                        $evaluacionN->save();
-                    }
-                }
-
-            flash('Se han creado los porcentajes ccon exito');
-
-            return redirect()->route('Pnotas.index');
-
-        }else{
-
-        flash('LA SUMATORIA NO ES DE 100', 'danger' );
-
-        $carrera = Carrera::find($request->IdCarrera);
-
-        $materias = Materia::orderBy('nombre','DESC')->lists('nombre','id');
-
-        $mis_materias = $carrera->materias->lists('nombre','id')->ToArray();
-
-        return view('Pnotas.create2')
-        ->with('carrera',$carrera)
-        ->with('mis_materias',$mis_materias);
-
-        }
-
+        dd($request->all());
     }
 
 
--- resources/views/Pnotas/create2.blade.php
+++ resources/views/Pnotas/create2.blade.php
@@ -43,55 +43,52 @@
 
            <div id ="NumeroEvaluaciones"  class="panel-body">
                 {!! form::label('numeroDeEvaluaciones', 'Numero de evaluaciones') !!}
-              <input name="numeroDeEvaluacones" type="number" id="numeroDeEvaluaciones" value="3"  class="form-control"  placeholder="5" />
+              <input name="numeroDeEvaluacones" type="number" value="3" id="numeroDeEvaluaciones" class="form-control"  placeholder="5" min="3" max="10" onchange="mostrar(this.value)" />
+
+              <input type="hidden" name="aumento" id="aumento" value="0">
 
           </div> 
-
-
-          <div class="form-group">
-              <input type="button" value="Añadir" onclick="mostrar()" class="btn-primary"> 
-          </div>
 
 
           <div class="table-responsive">
              <table class="table table-striped" > 
 
               <thead>
-                <th>Nota 1</th>
-                <th>Nota 2</th>
-                <th>Nota 3</th>
-                <th>Nota 4</th>
-                <th>Nota 5</th>
-                <th>Nota 6</th>
-                <th>Nota 7</th>
-                <th>Nota 8</th>
-                <th>Nota 9</th>
-                <th>Nota 10</th>
+                <th id="1" style=" text-align: center;">Nota 1</th>
+                <th id="2" style="text-align: center;">Nota 2</th>
+                <th id="3" style=" text-align: center;">Nota 3</th>
+                <th id="4" style="visibility: hidden; text-align: center;">Nota 4</th>
+                <th id="5" style="visibility: hidden; text-align: center;">Nota 5</th>
+                <th id="6" style="visibility: hidden; text-align: center;">Nota 6</th>
+                <th id="7" style="visibility: hidden; text-align: center;">Nota 7</th>
+                <th id="8" style="visibility: hidden; text-align: center;">Nota 8</th>
+                <th id="9" style="visibility: hidden; text-align: center;">Nota 9</th>
+                <th id="10" style="visibility: hidden; text-align: center;">Nota 10</th>
               </thead>
               <tbody>
                 <tr>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
-                    <td><input class="form-control" type="number" name="nota"  min="1" max="100" style="width: 90px;"> </td>
+                    <td><input class="form-control columna1" type="number" name="nota1"  min="1" max="100" ></td>
+                    <td><input class="form-control columna2" type="number" name="nota2"  min="1" max="100" ></td>
+                    <td><input class="form-control columna3" type="number" name="nota3"  min="1" max="100" ></td>
+                    <td><input class="form-control columna4" type="number" name="nota4"  min="1" max="100" ></td>
+                    <td><input class="form-control columna5" type="number" name="nota5"  min="1" max="100" ></td>
+                    <td><input class="form-control columna6" type="number" name="nota6"  min="1" max="100" ></td>
+                    <td><input class="form-control columna7" type="number" name="nota7"  min="1" max="100" ></td>
+                    <td><input class="form-control columna8" type="number" name="nota8"  min="1" max="100" ></td>
+                    <td><input class="form-control columna9" type="number" name="nota9"  min="1" max="100"></td>
+                    <td><input class="form-control columna10" type="number" name="nota10"  min="1" max="100"></td>
                 </tr>
                    <tr>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
-                    <td><input class="form-control" type="text" name="Descr" placeholder="Descripcion"  style="width: 90px;"> </td>
+                    <td><input class="form-control columna1" type="text" name="Descr1" placeholder="Descripcion" ></td>
+                    <td><input class="form-control columna2" type="text" name="Descr2" placeholder="Descripcion" ></td>
+                    <td><input class="form-control columna3" type="text" name="Descr3" placeholder="Descripcion" ></td>
+                    <td><input class="form-control columna4" type="text" name="Descr4" placeholder="Descripcion"></td>
+                    <td><input class="form-control columna5" type="text" name="Descr5" placeholder="Descripcion"></td>
+                    <td><input class="form-control columna6" type="text" name="Descr6" placeholder="Descripcion"></td>
+                    <td><input class="form-control columna7" type="text" name="Descr7" placeholder="Descripcion"></td>
+                    <td><input class="form-control columna8" type="text" name="Descr8" placeholder="Descripcion"></td>
+                    <td><input class="form-control columna9" type="text" name="Descr9" placeholder="Descripcion"></td>
+                    <td><input class="form-control columna10" type="text" name="Descr10" placeholder="Descripcion"></td>
                 </tr>
              </tbody>
             </table>
@@ -160,23 +157,68 @@
 
 @section('js')
   <script type="text/javascript">
-      function mostrar($id){
 
-        
+  
+  function mostrar($id){   
+           //codigo para cuando aumente las evaluaciones
+        var au = document.getElementById('aumento').value;
         var v = document.getElementById('numeroDeEvaluaciones').value;
-         
+        
+
         if (v<3 || v>10) {
-            //alert("El numero minimo de evaluaciones es 3 y el maximo 10");
+            alert("El numero minimo de evaluaciones es 3 y el maximo 10");
         }else{
-            for (paso = 1; paso <=v; paso++) {
-            document.getElementById(paso).style.display = 'block';
-          }
-        }
 
-      
-      };
-      
+            if (au <= v ) {
+              document.getElementById('aumento').value = v;
+        
+                var t = 300 - (v*21);
 
+                 for (var i = 1; i <= v; i++) {
+                   if(i<4){         
+                        $(".columna"+i).css("width", t+"px");     
+                  }else{
+                        $(".columna"+i).css("width", t+"px");
+
+                        var z = $(".columna"+i).css("display");                          
+                          if (z == "block") {
+
+                          }else{
+                            $(".columna"+i).css("display", "block");
+                            document.getElementById(i).style.visibility = 'visible';
+                          }
+                                          
+                    
+                  }
+                }
+                //alert("entro en aumento");   
+              }else{//aca termina la parte de aumento
+                document.getElementById('aumento').value = v;
+                var t = 90 + ((10-v) *21);
+
+                    for (var i = 10; i >v; i--) {
+                      var c = 11-i;
+                   if(i<4){         
+                        $(".columna"+c).css("width", t+"px");     
+                    }else{
+                        $(".columna"+c).css("width", t+"px");
+
+                        var z = $(".columna"+i).css("display");                          
+                          if (z == "none") {
+
+                          }else{
+                            $(".columna"+i).css("display", "none");
+                            document.getElementById(i).style.visibility = 'hidden';
+                          }
+                                          
+                    
+                  }
+                     
+                }
+                //alert("entro en dismuncion"); 
+                }
+      }
+    }   
 
   </script>
 
