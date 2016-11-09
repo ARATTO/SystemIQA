9fb1a2dd62c5133be6b91312a1eea12e078858ef
Dario Motto <dario_aratto@hotmail.com>
2016-11-08 23:13:36 -0600

    Antes de Merge->rodrigo

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
 <html>
-<head>
-	@include('template.partials.head')
-	<link rel="shortcut icon" href="{{ asset('dist/img/systemiqa/eternoslimpio.jpg') }}">
-</head>
-<body class="hold-transition skin-blue sidebar-mini">
-	@include('template.partials.nav')
-
-	<section>
-
-		@yield('content')
-
-	</section>
-
-	<footer class="main-footer">
-	  <div class="pull-right hidden-xs">
-	    <b>Version SystemIQA</b> 1.5
-	  </div>
-	  <div class="pull-left">
-	<img src="{{ asset('dist/img/systemiqa/UES.ico')}}"  class="pull-left  img-rounded img-responsive " height="25" width="25	" alt="Responsive image">
-
-	  </div>
-	  <strong>Copyright &copy; 2016 - <a href="#">Universidad de El Salvador</a>.</strong>  FIA-UES Todos los derechos reservados.
-
-	</footer>
+    <head>
+        @include('template.partials.head')
+        <link rel="shortcut icon" href="{{ asset('dist/img/systemiqa/eternoslimpio.jpg') }}">
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+        @include('template.partials.nav')
 
 
-	@include('template.partials.import_script')
-	@include('template.partials.load')
 
-	@yield('js')
+        <section>
+
+            @yield('content')
+
+        </section>
+
+        <footer class="main-footer">
+            <div class="pull-right hidden-xs">
+                <b>Version SystemIQA</b> 1.5
+            </div>
+            <div class="pull-left">
+                <img src="{{ asset('dist/img/systemiqa/UES.ico')}}"  class="pull-left  img-rounded img-responsive " height="25" width="25	" alt="Responsive image">
+
+            </div>
+            <strong>Copyright &copy; 2016 - <a href="#">Universidad de El Salvador</a>.</strong>  FIA-UES Todos los derechos reservados.
+
+        </footer>
 
 
-</body>
+        @include('template.partials.import_script')
+        @include('template.partials.load')
+
+        @yield('js')
+
+
+    </body>
 </html>
